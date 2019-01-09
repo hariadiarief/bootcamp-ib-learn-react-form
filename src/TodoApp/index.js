50d9d346561f82cdaabd59c04001657e761f6923
masagus hariadi arief <adi.arief77@gmail.com>
2019-01-10 00:09:06 +0700

    add headler in todoApp

--- src/TodoApp/index.js
+++ src/TodoApp/index.js
@@ -1,12 +1,68 @@
 import React, { Component } from "react";
 
 class TodoApp extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: "",
+      todos: [
+        {
+          id: 0,
+          text: "Eat Lunch"
+        },
+        {
+          id: 1,
+          text: "coding for fun"
+        }
+      ]
+    };
+  }
+
+  handleTodoInput = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  handleTodoSubmit = event => {
+    event.preventDefault();
+    console.log("TODO SUBMITED!", this.state.input);
+
+    const newTodo = {
+      id: this.state.todos.length,
+      text: this.state.input
+    };
+
+    const newTodos = this.state.todos.concat(newTodo);
+
+    this.setState({
+      input: "",
+      todos: newTodos
+    });
+  };
+
   render() {
     return (
       <div>
-        <h1>ini bakal jadi todo</h1>
-        <input type="text" placeholder="Input your todo hire" />
-        <input type="button" value="Register" />
+        <h1>This is Todo</h1>
+        <form onSubmit={this.handleTodoSubmit}>
+          <input
+            type="text"
+            placeholder="Input your todo hire"
+            onChange={this.handleTodoInput}
+            value={this.state.input}
+          />
+          <input type="submit" value="Add Todo" />
+        </form>
+
+        <ul>
+          {this.state.todos.map((todo, index) => {
+            return (
+              <li key={index}>
+                {todo.id}:{todo.text}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
