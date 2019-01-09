74e8f6db60952a18b0027472c18c421c175ddbab
masagus hariadi arief <adi.arief77@gmail.com>
2019-01-09 19:43:46 +0700

    Design TodoApp without handler

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Register from "./Register";
+import TodoApp from "./TodoApp";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Register />
+        <TodoApp />
       </div>
     );
   }
---
+++ src/TodoApp/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ini bakal jadi todo</h1>
+        <input type="text" placeholder="Input your todo hire" />
+        <input type="button" value="Register" />
+      </div>
+    );
+  }
+}
+export default TodoApp;
